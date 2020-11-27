3f3d71111371a0dadc9f01f0f560dce2ded047ca
salva09 <salva.hg01@gmail.com>
2020-11-26 21:39:14 -0600

    Remove unused method

--- src/main/kotlin/me/salva/Main.kt
+++ src/main/kotlin/me/salva/Main.kt
@@ -1,10 +1,8 @@
 package me.salva
 
-import com.formdev.flatlaf.FlatLightLaf
 import javax.swing.SwingUtilities
 
 fun main() {
-    FlatLightLaf.install()
     SwingUtilities.invokeLater {
         Home.isVisible = true
     }
