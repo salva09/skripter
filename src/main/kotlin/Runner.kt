2e84a947b26ea74215b46fcd515ab9ee906e6fea
salva09 <salva.hg01@gmail.com>
2020-11-26 18:25:36 -0600

    Add inherit all IO

--- src/main/kotlin/Runner.kt
+++ src/main/kotlin/Runner.kt
@@ -3,6 +3,7 @@ import java.util.concurrent.TimeUnit
 object Runner {
     fun run(script: String) {
         // Runs the command in other thread so we can edit while is running
+        // also kotlin scripts right now are kinda slow, would be nice to try to improve them
         val thread = Thread {
             "kotlinc -script $script".runCommand()
         }
@@ -11,8 +12,7 @@ object Runner {
 
     private fun String.runCommand() {
         ProcessBuilder(*split(" ").toTypedArray())
-            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
+            .inheritIO()
             .start()
             .waitFor(60, TimeUnit.MINUTES)
     }
