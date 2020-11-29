376794b806064ec5d545a756ca5594f787e4d3fb
salva09 <salva.hg01@gmail.com>
2020-11-29 02:57:52 -0600

    Remove unnecessary module

---
+++ src/FileManager.kt
@@ -0,0 +1,12 @@
+import java.io.File
+import java.io.IOException
+import javax.swing.JFileChooser
+import javax.swing.JMenuItem
+import javax.swing.JOptionPane
+
+object FileManager {
+    private val supportedExtensions = listOf("kts", "swift", "py")
+
+    fun newFile() {
+    }
+}
