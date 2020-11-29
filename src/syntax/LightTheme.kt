766c8c6b95cebf31d50e309c4bc755f4a008ffc4
salva09 <salva.hg01@gmail.com>
2020-11-29 13:11:02 -0600

    Remove try and catch block

--- src/syntax/LightTheme.kt
+++ src/syntax/LightTheme.kt
@@ -6,10 +6,8 @@ import org.fife.ui.rsyntaxtextarea.Token
 import org.fife.ui.rtextarea.RTextScrollPane
 import java.awt.Color
 import javax.swing.JFrame
-import javax.swing.JOptionPane
 import javax.swing.SwingUtilities
 import javax.swing.UIManager
-import kotlin.system.exitProcess
 
 class LightTheme : Theme {
     override val background: Color = Color(255, 255, 255)
@@ -20,19 +18,8 @@ class LightTheme : Theme {
     private val blue = Color(176, 196, 222)
 
     override fun setFrameLookAndFeel(frame: JFrame) {
-        try {
-            UIManager.setLookAndFeel(FlatLightLaf())
-            SwingUtilities.updateComponentTreeUI(frame)
-        } catch (ex: Exception) {
-            JOptionPane.showMessageDialog(
-                frame,
-                "Oh no! There was an error :( \n" +
-                    "Error: ${ex.message}",
-                "Error",
-                JOptionPane.ERROR_MESSAGE
-            )
-            exitProcess(0)
-        }
+        UIManager.setLookAndFeel(FlatLightLaf())
+        SwingUtilities.updateComponentTreeUI(frame)
     }
     override fun setTextAreaTheme(textArea: RSyntaxTextArea) {
         // Color schemes
