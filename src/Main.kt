1b1e2599bd6c2abe2a0719a034cb3d6b94240f67
salva09 <salva.hg01@gmail.com>
2020-11-29 12:46:52 -0600

    Change main function to a main class

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+import syntax.LightTheme
+
+lateinit var view: Home
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            view = Home(LightTheme())
+        }
+    }
+}
--- src/main.kt
+++
@@ -1,7 +0,0 @@
-import syntax.LightTheme
-
-lateinit var view: Home
-
-fun main() {
-    view = Home(LightTheme())
-}
